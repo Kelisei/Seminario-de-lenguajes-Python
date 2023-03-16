67d7c5e8c5a65dc790ab76c34513c217eb9fd821
Francisco Percara <frankestudios71@gmail.com>
2023-03-16 08:45:35 -0300

    Solucion

---
+++ game.py
@@ -0,0 +1,45 @@
+from random import choice, randrange
+from datetime import datetime
+# Operadores posibles
+operators = ["+", "-", "*", "/"]
+# Cantidad de cuentas a resolver
+times = 5
+# Contador inicial de tiempo.
+# Esto toma la fecha y hora actual.
+init_time = datetime.now()
+exitos = 0
+fallos = 0
+print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
+for i in range(0, times):
+    # Se eligen números y operador al azar
+    number_1 = randrange(10)
+    number_2 = randrange(10)
+    operator = choice(operators)
+    # Se imprime la cuenta.
+    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?")
+    # Le pedimos al usuario el resultado
+    result = int(input("resultado: "))
+    match(operator):
+        case("+"):
+            aux = (number_1 + number_2)
+        case("-"):
+            aux = (number_1 - number_2)
+        case("*"):
+            aux = (number_1 * number_2)
+        case("/"):
+            aux = (number_1 // number_2)
+    print(aux)
+    if(result == aux):
+        print('Es correcto')
+        exitos += 1
+    else:
+        print('Es incorrecto')
+        fallos += 1
+# Al terminar toda la cantidad de cuentas por resolver.
+# Se vuelve a tomar la fecha y la hora.
+end_time = datetime.now()
+# Restando las fechas obtenemos el tiempo transcurrido.
+total_time = end_time - init_time
+# Mostramos ese tiempo en segundos.
+print(f"\nTardaste {total_time.seconds} segundos.")
+print('Exitos: ', exitos, 'Fallos: ', fallos)
